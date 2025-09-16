99ded41750dfddfd72fd629febce1da50328585a
Projetos <32280059+marcopimenta@users.noreply.github.com>
2025-09-16 06:36:42 -0300

    Adicionar client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import AgendaPage from "./pages/Agenda";
+import Prontuarios from "./pages/Prontuarios";
+import PatientDetail from "./pages/PatientDetail";
+import Gestao from "./pages/Gestao";
+import Configuracoes from "./pages/Configuracoes";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Index />} />
+          <Route path="/agenda" element={<AgendaPage />} />
+          <Route path="/prontuarios" element={<Prontuarios />} />
+          <Route path="/prontuarios/:id" element={<PatientDetail />} />
+          <Route path="/gestao" element={<Gestao />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
